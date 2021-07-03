52b6dd4649eeea840eb01b55b7f6a3e058ef5446
Roel Thijs <roel.thijs+github@gmail.com>
2021-07-03 17:51:53 +0200

    voorbeeld 8 bit mario

---
+++ snippets/8-bit_voorbeeld.rb
@@ -0,0 +1,17 @@
+use_bpm 200
+
+with_fx :bitcrusher do
+  play [:d3, :fs4, :e5]
+  sleep 0.5
+  play [:d3, :fs4, :e5]
+  sleep 1
+  play [:d3, :fs4, :e5]
+  sleep 1
+  play [:d3, :fs4, :c5]
+  sleep 0.5
+  play [:d3, :fs4, :e5]
+  sleep 1
+  play [:g4, :b4, :g5]
+  sleep 2
+  play [:g3, :g4]
+end
